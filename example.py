cc3f1984effc3631d499fe021896f8db3452b9ed
ludeeus <joasoe@gmail.com>
2018-10-28 20:05:29 +0100

    update example.py

--- example.py
+++ example.py
@@ -4,11 +4,14 @@ import json
 import aiohttp
 from pytautulli import Tautulli
 
+HOST = '192.168.2.11'
+PORT = '8181'
+API_KEY = '13838dcabfc34f45a6152897fb84c29a'
 
 async def test():
     """Example usage of pytautulli."""
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
-        data = Tautulli('192.168.2.11', '8181', '13838dcabfc34f45a6152897fb84c29a', LOOP, session, True)
+    async with aiohttp.ClientSession() as session:
+        data = Tautulli(HOST, PORT, API_KEY, LOOP, session, True)
         await data.test_connection()
         await data.get_data()
 
